6b3a695cbb16ce7bc88d6f01ba2ae996d8bd18ac
amnesia <amnesia@2806-107e-0014-182a-92a5-c1aa-815f-84e3.ipv6.infinitum.net.mx>
2024-04-25 19:30:51 -0600

    git init

---
+++ src/main/java/org/example/Automovil.java
@@ -0,0 +1,59 @@
+package org.example;
+
+public class Automovil {
+    private String fabricante;
+    private String modelo;
+    private String color;
+    private int capacidadEstanque = 40;
+    private double cilindrada;
+
+    public Automovil() {
+
+    }
+
+    public Automovil(String fabricante, String modelo, String color) {
+        this(fabricante, modelo);
+        this.color = color;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getCapacidadEstanque() {
+        return capacidadEstanque;
+    }
+
+    public void setCapacidadEstanque(int capacidadEstanque) {
+        this.capacidadEstanque = capacidadEstanque;
+    }
+
+    public double getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(double cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+}
+
