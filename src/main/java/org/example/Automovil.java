2582c8512acafadf8f276f3851e2faa84869e17a
amnesia <amnesia@2806-107e-0014-182a-92a5-c1aa-815f-84e3.ipv6.infinitum.net.mx>
2024-04-29 20:25:39 -0600

    Enums

--- src/main/java/org/example/Automovil.java
+++ src/main/java/org/example/Automovil.java
@@ -5,15 +5,34 @@ public class Automovil {
     private String modelo;
     private String color;
     private int capacidadEstanque = 40;
+    private int id;
     private double cilindrada;
 
-    public Automovil() {
+    private static int ultimoId;
 
+    public Automovil() {
+        this.id = ++ultimoId;
+    }
+
+    public Automovil(String fabricante, String modelo) {
+        this();
+        this.fabricante = fabricante;
+        this.modelo = modelo;
     }
 
     public Automovil(String fabricante, String modelo, String color) {
         this(fabricante, modelo);
         this.color = color;
+    }
+
+    public Automovil(String fabricante, String modelo, String color, int capacidadEstanque) {
+        this(fabricante, modelo, color);
+        this.capacidadEstanque = capacidadEstanque;
+    }
+
+    public Automovil(String fabricante, String modelo, String color, int capacidadEstanque, double cilindrada) {
+        this(fabricante, modelo, color, capacidadEstanque);
+        this.cilindrada = cilindrada;
     }
 
     public String getFabricante() {
@@ -52,8 +71,16 @@ public class Automovil {
         return cilindrada;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        Automovil a = (Automovil) obj;
+        return super.equals(this.fabricante.equals(a.getFabricante()) && this.modelo.equals(a.getModelo()));
+    }
+
     public void setCilindrada(double cilindrada) {
         this.cilindrada = cilindrada;
+
+
     }
 }
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(Color.BLANCO.getColor());
+        System.out.println(TipoAutomovil.CONVERTIBLE.getDesc());
 
+        TipoAutomovil[] tipos = TipoAutomovil.values();
+
+        for (TipoAutomovil tipoAutomovil : tipos) {
+            System.out.println(tipoAutomovil.getNombre() );
+        }
     }
 }
